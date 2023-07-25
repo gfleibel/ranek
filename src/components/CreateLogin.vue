afd89cd6fb2e34616eea636af04e81754b00e85b
gabriel leibel <gfleibel@hotmail.com>
2023-07-25 17:50:06 -0300

    created signup form with post user data

--- src/components/CreateLogin.vue
+++ src/components/CreateLogin.vue
@@ -1,11 +1,58 @@
 <template>
-<p>Login Criar</p>
+  <section>
+    <h2>Crie a sua conta</h2>
+    <transition mode="out-in">
+      <button v-if="!create" class="btn" @click="create = true">Criar Conta</button>
+      <UserForm v-else>
+        <button class="btn btn-form" @click.prevent="createUser">Criar Usuário</button>
+      </UserForm>
+    </transition>
+  </section>
 </template>
 
 <script>
-export default{
+import UserForm from '@/components/UserForm.vue'
+
+export default {
   name: 'CreateLogin',
+  data() {
+    return {
+      create: false
+    }
+  },
+  components: {
+    UserForm
+  },
+  methods: {
+    async createUser() {
+      try {
+        await this.$store.dispatch('createUser', this.$store.state.user);
+        await this.$store.dispatch('getUser', this.$store.state.user.email);
+        this.$router.push({ name: "user" });
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+  }
 }
 </script>
 
-<style></style>
+<style scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.btn-form {
+  max-width: 100%;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: true,
   state: {
     login: false,
     user: {
@@ -25,15 +26,19 @@ export default new Vuex.Store({
       state.login = payload;
     },
     UPDATE_USER(state, payload) {
-      state.user = payload;
+      state.user = Object.assign(state.user, payload);
     },
   },
   actions: {
     getUser(context, payload) {
-      api.get(`/users/${payload}`).then((response) => {
+      return api.get(`/user/${payload}`).then((response) => {
         context.commit("UPDATE_USER", response.data);
         context.commit("UPDATE_LOGIN", true);
       });
+    },
+    createUser(context, payload) {
+      context.commit("UPDATE_USER", { id: payload.email });
+      return api.post("/user", payload);
     },
   },
   modules: {},
