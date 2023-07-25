5ea9c39112c0bf299d5e7c1f4e96e8d68867ba81
gabriel leibel <gfleibel@hotmail.com>
2023-07-25 10:25:17 -0300

    configured search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,23 @@
 <template>
-  <div></div>
+  <form>
+    <input type="text" name="search" id="search" v-model="search">
+    <input type="submit" id="magnifier" value="Buscar" @click.prevent="searchProducts">
+  </form>
 </template>
 
 <script>
 export default {
-  name: "SearchBar"
+  name: "SearchBar",
+  data() {
+    return {
+      search: "",
+    };
+  },
+  methods: {
+    searchProducts() {
+      this.$router.push({ query: { q: this.search } })
+    }
+  }
 }
 </script>
 
