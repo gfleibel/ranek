5b6bc08b667562ed0e3d0b58d92505d5d3241d38
gabriel leibel <gfleibel@hotmail.com>
2023-07-26 10:38:20 -0300

    created user view with add and remove product features and edit user

---
+++ src/components/AddProduct.vue
@@ -0,0 +1,56 @@
+<template>
+  <form class="add-product">
+    <label for="name">Nome</label>
+    <input type="text" id="name" name="name" v-model="product.name">
+    <label for="price">Preço (R$)</label>
+    <input type="number" id="price" name="price" v-model="product.price">
+    <label for="photos">Fotos</label>
+    <input type="file" id="photos" name="photos" ref="photos">
+    <label for="description">Descrição</label>
+    <textarea id="description" name="description" v-model="product.description"></textarea>
+    <input class="btn" type="button" value="Adicionar Produto" @click.prevent="addProduct">
+  </form>
+</template>
+
+<script>
+import { api } from '@/services.js';
+
+export default {
+  name: 'AddProduct',
+  data() {
+    return {
+      product: {
+        name: '',
+        description: '',
+        price: '',
+        photos: null
+      }
+    }
+  },
+  methods: {
+    formatProduct() {
+      this.product.user_id = this.$store.state.user.id;
+    },
+    addProduct() {
+      this.formatProduct();
+      api.post('/products', this.product).then(() => {
+        this.$store.dispatch('getUserProducts');
+      });
+    },
+  }
+}
+</script>
+
+<style scoped>
+.add-product {
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  align-items: center;
+  margin-bottom: 60px;
+}
+
+.btn {
+  grid-column: 2;
+
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,12 @@ import HomeView from "../views/HomeView.vue";
 import ProductView from "../views/ProductView.vue";
 import LoginView from "../views/LoginView.vue"
 import UserView from "../views/User/UserView.vue"
+import UserProducts from "../views/User/UserProducts.vue"
+import UserSales from "../views/User/UserSales.vue"
+import UserPurchases from "../views/User/UserPurchases.vue"
+import UserEdit from "../views/User/UserEdit.vue"
+
+
 
 Vue.use(VueRouter);
 
@@ -26,8 +32,29 @@ const routes = [
   },
   {
     path: "/user",
-    name: "user",
     component: UserView,
+    children: [
+      {
+        path:"",
+        name: "user",
+        component: UserProducts,
+      },
+      {
+        path:"purchases",
+        name: "purchases",
+        component: UserPurchases,
+      },
+      {
+        path:"sales",
+        name: "sales",
+        component: UserSales,
+      },
+      {
+        path:"edit",
+        name: "edit-user",
+        component: UserEdit,
+      }
+    ]
   },
 ];
 
